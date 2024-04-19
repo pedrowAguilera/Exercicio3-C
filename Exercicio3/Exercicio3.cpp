ca1c92f5bb1cf3e690d3420d9789a9789f7177d1
ppz1 <133365100+pedrowAguilera@users.noreply.github.com>
2024-04-19 15:26:36 -0300

    Initial commit

---
+++ Exercicio3/Exercicio3.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+
+using namespace std;
+
+// EXERCICIO 1
+//int main() {
+//	int numero1;
+//	int numero2;
+//
+//	cout << "Insira um numero: ";
+//	cin >> numero1;
+//
+//	cout << "Insira outro numero: ";
+//	cin >> numero2;
+//
+//	if (numero1 > numero2) {
+//		cout << "O primeiro numero e maior";
+//	}
+//	else if (numero1 < numero2) {
+//		cout << "O segundo numero e maior";
+//	}
+//	else {
+//		cout << "Os numeros sao iguais";
+//	}
+// 
+//}
+
+
+// EXERCICIO 2
+//int main() {
+//	int quantidade;
+//	cout << "Informe a quantidade de produtos que tem no estoque: ";
+//	cin >> quantidade;
+//
+//	if (quantidade >= 100) {
+//		cout << "Quantidade suficiente no estoque";
+//	}
+//	else if (quantidade >= 50) {
+//		cout << "Atencao ao estoque";
+//	}
+//	else {
+//		cout << "Faca um novo pedido e reabasteca o estoque";
+//	}
+//
+//}
+
+
+// EXERCICIO 3
+//int main() {
+//	int numero;
+//	cout << "Informe um numero: ";
+//	cin >> numero;
+//
+//	if (numero > 0) {
+//		cout << "O numero e positivo";
+//	}
+//	else if (numero < 0) {
+//		cout << "O numero e negativo";
+//	}
+//	else {
+//		cout << "O numero e igual a zero";
+//	}
+//}
+
+
+/*
+DESAFIO
+SEM USAR CALCULOS, O QUE É PRECISO FAZER PARA QUE AO INVÉS DE NO CONSOLE APARERER 5 E 23, APAREÇA 23 E 5
+
+	int a = 5;
+	int b = 23;
+	cout << a;
+	cout << b;
+
+	SOLUÇÃO:
+
+	int main() {
+	int a = 5;
+	int b = 23;
+
+	int fake = a = 23;
+	int fake2 = b = 5;
+
+	cout << a;
+	cout << "\n";
+	cout << b;
+
+}
+
+*/
+
